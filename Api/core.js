09a67bcf782483f868dceae00a92390fb1b95e06
DuvanCamiloArenasRodriguez <dccar21@gmail.com>
2024-03-13 08:20:45 -0500

    styles: :lipstick: Adición de estilos
    Se añade estilos a cada componente

---
+++ Api/core.js
@@ -0,0 +1,5 @@
+// const URL = "http://154.38.171.54:3000/"
+
+// export async function consultarDatos(endpoint) {
+    
+// }
---
+++ js/app.js
@@ -0,0 +1,12 @@
+// import {  } from "core.js";
+
+const empezar = document.querySelector("#btnEmpezar")
+
+
+empezar.addEventListener("click", () => {
+    const pantallaInicial = document.querySelector(".pantallaInicial")
+    console.log("leyendo leyendo");
+    pantallaInicial.style.display = 'none'
+})
+
+const nivelCalidad = document.querySelector("")
